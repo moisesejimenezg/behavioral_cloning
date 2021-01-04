b65f949a392fe6edfbda4f03f4d67521c37d54ff
Moises Jimenez <moisesejimenezg@outlook.com>
2021-01-04 20:26:47 +0100

    Use Nvidia network

--- src/model.py
+++ src/model.py
@@ -1,4 +1,5 @@
-from keras.models import Sequential
+from keras.models import Sequential, load_model
+from keras.layers import Cropping2D
 from keras.layers.core import Dense, Activation, Flatten, Dropout, Lambda
 from keras.layers.convolutional import Conv2D, Convolution2D
 from keras.layers.pooling import MaxPooling2D
@@ -6,18 +7,23 @@ from keras.layers.pooling import MaxPooling2D
 class Model:
     def __init__(self, image_h=160, image_w=320, image_d=3):
         model = Sequential()
-        model.add(Lambda(lambda x: x/255, input_shape=(160, 320, 3)))
-        model.add(Convolution2D(6, 5, 5, activation="relu"))
-        model.add(MaxPooling2D())
-        model.add(Convolution2D(6, 5, 5, activation="relu"))
-        model.add(MaxPooling2D())
+        model.add(Lambda(lambda x: x/255 - 0.5, input_shape=(160, 320, 3)))
+        model.add(Cropping2D(cropping=((70, 25), (0, 0))))
+        model.add(Convolution2D(24, 5, 2, activation="relu"))
+        model.add(Convolution2D(36, 5, 2, activation="relu"))
+        model.add(Convolution2D(48, 5, 2, activation="relu"))
+        model.add(Convolution2D(64, 3, activation="relu"))
+        model.add(Convolution2D(64, 3, activation="relu"))
+        model.add(Dropout(0.3))
         model.add(Flatten())
-        model.add(Dense(120))
-        model.add(Dense(84))
+        model.add(Dense(100))
+        model.add(Dense(50))
+        model.add(Dense(10))
         model.add(Dense(1))
         model.compile(loss="mse", optimizer="adam")
         self.model = model
 
     def fit_model(self, x, y, validation_split=0.2, shuffle=True):
-        self.model.fit(x, y, validation_split=validation_split, shuffle=shuffle, epochs=10)
+        print("Fitting model with: " + str(len(x)) + " images.")
+        self.model.fit(x, y, validation_split=validation_split, shuffle=shuffle, epochs=5)
         self.model.save("model.h5")
